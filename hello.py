57b8e95a2da775100367bfd6ed29c9b130ffb94c
Anna Sivenko <fogsi@yandex.ru>
2022-05-10 21:11:37 +0300

    add age question

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 name = input('Как тебя зовут? ')
 message = 'Привет, ' + name + '.'
 print(message)
+response = input('В каком году ты родился?')
+year = int(response)
+age = 2022 - year
+print('Тебе ' + str(age) + ' лет.')
